b3c44978cf01aa8d2d52f1d2ba54024568fbb5db
ADMIN <ADMIN@LAB1-MAY31>
2023-09-11 11:16:04 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace scharp_bai6._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txthk1 = new System.Windows.Forms.TextBox();
+            this.txthk2 = new System.Windows.Forms.TextBox();
+            this.txtdtb = new System.Windows.Forms.TextBox();
+            this.txtxeploai = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(359, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "kết quả học tập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Điểm hk2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Điểm HK1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Điểm Trung Bình";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(141, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Kết  quả ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(141, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Xếp loại học lực";
+            // 
+            // txthk1
+            // 
+            this.txthk1.Location = new System.Drawing.Point(254, 96);
+            this.txthk1.Name = "txthk1";
+            this.txthk1.Size = new System.Drawing.Size(230, 20);
+            this.txthk1.TabIndex = 2;
+            // 
+            // txthk2
+            // 
+            this.txthk2.Location = new System.Drawing.Point(254, 138);
+            this.txthk2.Name = "txthk2";
+            this.txthk2.Size = new System.Drawing.Size(230, 20);
+            this.txthk2.TabIndex = 2;
+            // 
+            // txtdtb
+            // 
+            this.txtdtb.Location = new System.Drawing.Point(254, 176);
+            this.txtdtb.Name = "txtdtb";
+            this.txtdtb.Size = new System.Drawing.Size(230, 20);
+            this.txtdtb.TabIndex = 2;
+            // 
+            // txtxeploai
+            // 
+            this.txtxeploai.Location = new System.Drawing.Point(254, 260);
+            this.txtxeploai.Name = "txtxeploai";
+            this.txtxeploai.Size = new System.Drawing.Size(230, 20);
+            this.txtxeploai.TabIndex = 2;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(254, 213);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(230, 20);
+            this.txtkq.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Xem kết quả ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtxeploai);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txtdtb);
+            this.Controls.Add(this.txthk2);
+            this.Controls.Add(this.txthk1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txthk1;
+        private System.Windows.Forms.TextBox txthk2;
+        private System.Windows.Forms.TextBox txtdtb;
+        private System.Windows.Forms.TextBox txtxeploai;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scharp_bai6._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double hk1, hk2, dtb;
+            hk1 = Convert.ToDouble(txthk1.Text);
+            hk2 = Convert.ToDouble(txthk2.Text);
+            dtb = (hk1 + hk2) / 2;
+            txtdtb.Text = Convert.ToString(dtb);
+            if (dtb >= 5)
+            {
+                txtkq.Text = "được lên lớp";
+            } 
+            else 
+                if (dtb <=5)
+            {
+                txtkq.Text = " ở lại lớp ";
+            }
+            if (dtb < 5)
+            {
+                txtxeploai.Text = "yếu";
+            }
+            else if (dtb <= 6 )
+            {
+                txtxeploai.Text = " trung bình";
+            }
+            else if (dtb < 8 )
+            {
+                txtxeploai.Text = "giỏi";
+            }
+            
+
+        }
+    }
+}
